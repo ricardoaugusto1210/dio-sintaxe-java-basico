d9dc38003a66bb02e8b86e0ef04e96df9912aa70
Ricardo Augusto <ricardoaugusto.eb@gmail.com>
2024-04-29 21:19:05 -0300

    feat: implementação do scanner

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner sc = new Scanner(System.in);
+
+        double saldo = 237.48;
+
+        // Exibir as mensagens para o nosso usuário
+        System.out.println("Por favor, digite o número da sua conta");
+        int numeroConta = sc.nextInt();
+        System.out.println("Por favor, digite a sua agência");
+        String agencia = sc.next();
+        System.out.println("Por favor, digite o nome do seu cliente");
+        String nomeCliente = sc.next();
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, agencia, numeroConta, saldo);
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+
+        sc.close();
     }
 }
